1f1ba6ed17727904725ec5598aab3829f4099b2f
0xawang <tik594746@gmail.com>
2025-07-30 03:48:09 +0900

    fix: server side issue

--- src/pages/swap/index.js
+++ src/pages/swap/index.js
@@ -1,17 +1,23 @@
 import { Box } from '@mui/material'
+import { useState } from 'react'
 import { FullCenterBox } from 'src/components/base/grid'
 import { SWAP_WIDGET_CONFING } from 'src/components/swap/config'
 import SwapWidget from 'src/components/swap/widget'
 import { useHydrated } from 'src/hooks/useHydrated'
 
 const Swap = () => {
+  const [isClient, setIsClient] = useState(false)
   const hydrated = useHydrated()
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
 
   return (
     <section className='section_header-text-image'>
       <Box className='container-large'>
         <Box className='header-text-image_content'>
-          <FullCenterBox>{hydrated && <SwapWidget config={SWAP_WIDGET_CONFING} />}</FullCenterBox>
+          <FullCenterBox>{hydrated && isClient && <SwapWidget config={SWAP_WIDGET_CONFING} />}</FullCenterBox>
         </Box>
       </Box>
     </section>
