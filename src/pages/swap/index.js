fc0f9d93657ce2e9b4106d04ae604273ce6602c5
0xawang <tik594746@gmail.com>
2025-07-30 04:00:01 +0900

    fix: import dynamic swap widget

--- src/pages/swap/index.js
+++ src/pages/swap/index.js
@@ -1,23 +1,37 @@
 import { Box } from '@mui/material'
+import dynamic from 'next/dynamic'
 import { useState } from 'react'
 import { FullCenterBox } from 'src/components/base/grid'
 import { SWAP_WIDGET_CONFING } from 'src/components/swap/config'
-import SwapWidget from 'src/components/swap/widget'
 import { useHydrated } from 'src/hooks/useHydrated'
 
-const Swap = () => {
-  const [isClient, setIsClient] = useState(false)
-  const hydrated = useHydrated()
+const SwapWidget = dynamic(() => import('src/components/swap/widget'), {
+  ssr: false,
+  loading: () => (
+    <Box
+      sx={{
+        width: '100%',
+        height: '600px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0,0,0,0.05)',
+        borderRadius: 2
+      }}
+    >
+      Loading Swap Widget...
+    </Box>
+  )
+})
 
-  useEffect(() => {
-    setIsClient(true)
-  }, [])
+const Swap = () => {
+  const hydrated = useHydrated()
 
   return (
     <section className='section_header-text-image'>
       <Box className='container-large'>
         <Box className='header-text-image_content'>
-          <FullCenterBox>{hydrated && isClient && <SwapWidget config={SWAP_WIDGET_CONFING} />}</FullCenterBox>
+          <FullCenterBox>{hydrated && <SwapWidget config={SWAP_WIDGET_CONFING} />}</FullCenterBox>
         </Box>
       </Box>
     </section>
